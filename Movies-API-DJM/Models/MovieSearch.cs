fccb36cf9ea32178acbf7d20a8855450366870da
dakota <dakotaslabb@gmail.com>
2019-11-07 21:21:47 -0500

    More Models

---
+++ Movies-API-DJM/Models/MovieSearch.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace Movies_API_DJM.Models
+{
+    public class MovieSearch
+    {
+        public List<string> Title { get; set; } = new List<string>();
+
+        public MovieSearch()
+        {
+
+        }
+
+        public MovieSearch(JToken token)
+        {
+            foreach (var item in token["Search"])
+            {
+                Title.Add(item["Title"].ToString());
+            }
+        }
+    }
+}
